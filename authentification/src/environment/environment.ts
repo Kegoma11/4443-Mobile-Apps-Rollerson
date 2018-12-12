71902117fb610f3f0cfb5d512897ab6210406be2
Kegoma11 <keonarollerson@gmail.com>
2018-12-12 15:09:06 -0600

    Add files via upload

---
+++ authentification/src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  //production: false,
+  googleWebClientId:'998592988479-turmt4mbun9pp9rgg2k3aq1feic6iuro.apps.googleusercontent.com',
+  firebase: {
+    apiKey: "AIzaSyA5Q_Ta6kYdeGJyq9iC0nEqCaE3ElsoqMs",
+    authDomain: "my-location-c1b3b.firebaseapp.com",
+    databaseURL: "https://my-location-c1b3b.firebaseio.com",
+    projectId: "my-location-c1b3b",
+    storageBucket: "my-location-c1b3b.appspot.com",
+    messagingSenderId: "998592988479"
+  
+  }
+};
